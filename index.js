9b7a59ab734a9666a7f283553bcd9ecea8d73a82
codebreak0 <salinakarki00@gmail.com>
2020-06-08 17:27:44 +0545

    list repo name

---
+++ index.js
@@ -0,0 +1,5 @@
+// fetch('https://api.github.com/users/salina-k/repos?per_page=10')
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.log('error'))
+
