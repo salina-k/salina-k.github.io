30c940670f545071ac8a471d6ba05c9027dc35d3
codebreak0 <salinakarki00@gmail.com>
2020-06-09 19:19:52 +0545

    added repo links to text

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const ul = document.querySelector('#repos');
 // GitHub API URL
 const url = 'https://api.github.com/users/salina-k/repos?page=1&per_page=10';
 
+
 // make the API call
 fetch(url)
     .then(res => res.json())
@@ -23,11 +24,14 @@ fetch(url)
         data.map((user) => {
             // create the elements
             let li = createNode('li'),
-                span = createNode('span');
+                span = createNode('span'),
+                anchor = createNode('a');
             span.innerText = user.name;
+            anchor.href = repos.html_url; // create link to each repos
             // append all elements
             appendNode(li, span);
             appendNode(ul, li);
+            appendNode(span, anchor);
         });
     }).catch(err => {
         console.error('Error: ', err);
